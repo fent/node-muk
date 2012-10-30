29b006ee801511d854255f84295acce5fa3856f6
fent <933490+fent@users.noreply.github.com>
2012-10-30 03:16:54 -0700

    fix mocking requiring with a relative path

--- test/main-test.js
+++ test/main-test.js
@@ -47,22 +47,31 @@ describe('Mock methods', function() {
 });
 
 
-describe('Mock required dependency', function() {
-  var original = require('mocha');
+function testMockDependency(filename) {
+  var original = require(filename);
 
   it('Correctly mocks dependency', function() {
     var mockObject = { existsSync: function() { return true; } };
-    var mockedModule = muk('mocha', mockObject);
+    var mockedModule = muk(filename, mockObject);
 
     assert.equal(mockedModule, mockObject,
                  'returned module is mocked object');
-    assert.equal(require('mocha'), mockObject,
+    assert.equal(require(filename), mockObject,
                  'when require is called it returns the mocked object');
   });
 
   it('Restores original module when muk.restore() is called', function() {
     muk.restore();
-    assert.equal(require('mocha'), original,
+    assert.equal(require(filename), original,
                  'requiring module again returns orignal module');
   });
+}
+
+
+describe('Mock required user land dependency', function() {
+  testMockDependency('mocha');
+});
+
+describe('Mock required file', function() {
+  testMockDependency('./foo');
 });
